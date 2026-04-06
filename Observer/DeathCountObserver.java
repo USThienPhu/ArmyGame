a3f68328f05d30140b75f71d4655dce03319892c
23120327 <thaithienphu74109630@gmail.com>
2026-04-06 13:46:05 +0700

    singleton

--- Observer/DeathCountObserver.java
+++ Observer/DeathCountObserver.java
@@ -1,7 +1,11 @@
 // DeathCountObserver.java
 public class DeathCountObserver implements DeathObserver {
+    private static final DeathCountObserver instance = new DeathCountObserver();
     private int totalDeaths = 0;
-
+    private DeathCountObserver() {}
+    public static DeathCountObserver getInstance() {
+        return instance;
+    }
     @Override
     public void onSoldierDeath(String name) {
         totalDeaths++;
--- Observer/DeathNotifierObserver.java
+++ Observer/DeathNotifierObserver.java
@@ -1,4 +1,13 @@
+// DeathNotifierObserver.java
 public class DeathNotifierObserver implements DeathObserver {
+    private static final DeathNotifierObserver instance = new DeathNotifierObserver();
+
+    private DeathNotifierObserver() {}
+
+    public static DeathNotifierObserver getInstance() {
+        return instance;
+    }
+
     @Override
     public void onSoldierDeath(String name) {
         System.out.println("[DeathNotifier] ALERT: Unit '" + name + "' has died.");
@@ -6,7 +15,6 @@ public class DeathNotifierObserver implements DeathObserver {
     }
 
     private void sendMail(String name) {
-        // As requested: Just a print statement to terminal
         System.out.println(">> SENDING EMAIL: 'Dear friends of " + name + ", we deeply regret to inform you of their heroic passing.'");
     }
 }
